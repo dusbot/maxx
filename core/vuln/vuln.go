3895cd896003b50ee3ce41f4f1429e78da3d1ba3
1dk <3520124658@qq.com>
2025-07-04 15:49:15 +0800

    vuln update

--- core/vuln/vuln.go
+++ core/vuln/vuln.go
@@ -4,9 +4,17 @@ import (
 	"context"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
 
+	"github.com/dusbot/cpe2cve/core"
 	"github.com/dusbot/maxx/libs/common"
+	"github.com/dusbot/maxx/libs/finger"
 	"github.com/dusbot/maxx/libs/slog"
+	"github.com/dusbot/maxx/libs/uhttp"
+	"github.com/dusbot/maxx/libs/uslice"
 	"github.com/dusbot/maxx/libs/utils"
 	nuclei "github.com/projectdiscovery/nuclei/v3/lib"
 	"github.com/projectdiscovery/nuclei/v3/pkg/output"
@@ -19,6 +27,11 @@ var (
 	TEMPLATE_CUSTOM_FOLDER = "custom"
 	NUCLEI_BINARY          = "nuclei"
 	TEMPLATES_URL          = "https://github.com/dusbot/templates"
+
+	PingProgressRate       = 10
+	CommonTaskProgressRate = 25
+	CPETaskProgressRate    = 25
+	NucleiProgressRate     = 40
 )
 
 type Engine struct {
@@ -26,6 +39,17 @@ type Engine struct {
 	TemplateInnerPath  string
 	TemplateCustomPath string
 	Templates          []*templates.Template
+}
+
+type TaskStat struct {
+	ProgressPipe           chan int
+	ResultPipe             chan VulnResult
+	ProgressPipeClosed     atomic.Bool
+	ResultPipeClosed       atomic.Bool
+	PingTaskProgress       *atomic.Int32
+	NucleiTaskProgress     *atomic.Int32
+	CPETaskRateProgress    *atomic.Int32
+	CommonTaskRateProgress *atomic.Int32
 }
 
 func (e *Engine) AddTemplate(fileName string, buf []byte) (err error) {
@@ -65,55 +89,232 @@ func (e *Engine) UpdateTemplates() error {
 	return utils.DownloadAndExtractLatestRelease(TEMPLATES_URL, e.TemplateInnerPath, false)
 }
 
-type Target struct {
-	Urls []string
-
-	TemplateIDs []string
-	Tags        []string
-	Proxies     []string
+type TargetItem struct {
+	Url  string
+	Tags []string
+	CPEs []string
 }
 
-func (e *Engine) Scan(t Target) (results []*output.ResultEvent) {
-	if len(t.Urls) == 0 || (len(t.Tags) == 0 && len(t.TemplateIDs) == 0) {
-		slog.Printf(slog.WARN, "Target is empty, urls:%+v, tags:%+v, templateIDs:%+v", t.Urls, t.Tags, t.TemplateIDs)
+type VulnTask struct {
+	TargetItems []TargetItem
+
+	Proxies []string
+
+	TemplateIDs []string
+
+	Stat *TaskStat
+}
+
+const (
+	TYPE_COMMON byte = iota
+	TYPE_CPE
+	TYPE_NUCLEI
+)
+
+type VulnResult struct {
+	Type          byte
+	CommonResults []*CommonResult
+	NucleiResults []*output.ResultEvent
+	CPEResults    []*CPEResult
+}
+
+type CommonResult struct {
+	Name    string
+	Url     string
+	Method  string
+	Payload string
+	Proof   string
+}
+
+type CPEResult struct {
+	Url  string
+	CVEs []string
+}
+
+func InitTaskStat() *TaskStat {
+	return &TaskStat{
+		ProgressPipe:           make(chan int, 1<<10),
+		ResultPipe:             make(chan VulnResult, 1<<10),
+		ProgressPipeClosed:     atomic.Bool{},
+		ResultPipeClosed:       atomic.Bool{},
+		PingTaskProgress:       &atomic.Int32{},
+		NucleiTaskProgress:     &atomic.Int32{},
+		CPETaskRateProgress:    &atomic.Int32{},
+		CommonTaskRateProgress: &atomic.Int32{},
+	}
+}
+
+func (e *Engine) Scan(t VulnTask) {
+	if t.Stat == nil {
+		t.Stat = InitTaskStat()
+	}
+	go func() {
+		for {
+			progress := t.Stat.PingTaskProgress.Load() + t.Stat.NucleiTaskProgress.Load() + t.Stat.CPETaskRateProgress.Load() + t.Stat.CommonTaskRateProgress.Load()
+			if progress >= 99 {
+				progress = 99
+			}
+			go func() {
+				t.Stat.ProgressPipe <- int(progress)
+			}()
+			time.Sleep(time.Second * 5)
+		}
+	}()
+	deferClearFunc := func() {
+		time.Sleep(time.Second * 5)
+		go func() {
+			for _ = range t.Stat.ProgressPipe {
+			}
+		}()
+		go func() {
+			for _ = range t.Stat.ResultPipe {
+			}
+		}()
+		t.Stat.ProgressPipeClosed.Store(true)
+		t.Stat.ResultPipeClosed.Store(true)
+		close(t.Stat.ProgressPipe)
+		close(t.Stat.ResultPipe)
+	}
+	deferFunc := func() {
+		go deferClearFunc()
+		t.Stat.ProgressPipe <- 100
+	}
+	defer deferFunc()
+	var activeItems []TargetItem
+	// stage1: alive, tags, cpes and the stage progress
+	for index, item := range t.TargetItems {
+		t.Stat.PingTaskProgress.Store(int32((index + 1) * PingProgressRate / len(t.TargetItems)))
+		// http alive
+		if strings.HasPrefix(item.Url, "http") || strings.HasPrefix(item.Url, "https") {
+			if _, header, body, err := uhttp.GET(uhttp.RequestInput{
+				RawUrl:  item.Url,
+				Proxy:   uslice.GetRandomItem(t.Proxies),
+				Timeout: time.Second * 5,
+			}); err == nil {
+				if len(item.Tags) == 0 {
+					item.Tags = finger.Engine.Match(header, body)
+				}
+				activeItems = append(activeItems, TargetItem{
+					Url:  item.Url,
+					Tags: item.Tags,
+					CPEs: item.CPEs,
+				})
+			} else {
+				slog.Printf(slog.WARN, "Vuln task Ping[%s] with error:%+v", item.Url, err)
+			}
+		}
+		//other protocol: todo...
+
+	}
+	// stage2: Run all tasks in parallel
+	var wg = &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		e.ExecuteNuclei(activeItems, t.TemplateIDs, t.Proxies, t.Stat.ResultPipe, t.Stat.NucleiTaskProgress)
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		e.ExecuteCPE(activeItems, t.Stat.ResultPipe, t.Stat.CPETaskRateProgress)
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		t.Stat.CommonTaskRateProgress.Store(int32(CommonTaskProgressRate))
+	}()
+
+	// stage3: wait collect results
+	wg.Wait()
+}
+
+func (e *Engine) ExecuteNuclei(
+	targetItems []TargetItem,
+	templateIDs, proxies []string,
+	resultPipe chan VulnResult,
+	nucleiProgressRate *atomic.Int32,
+) {
+	if len(targetItems) == 0 {
+		slog.Printf(slog.WARN, "TargetItem is empty")
 		return
 	}
-	var tplFilters nuclei.TemplateFilters
-	if len(t.Tags) > 0 {
-		tplFilters.Tags = t.Tags
+	for index, t := range targetItems {
+		nucleiProgress := (index + 1) * NucleiProgressRate / len(targetItems)
+		nucleiProgressRate.Store(int32(nucleiProgress))
+		var tplFilters nuclei.TemplateFilters
+		if len(templateIDs) > 0 {
+			tplFilters.IDs = templateIDs
+		} else {
+			if len(t.Tags) > 0 {
+				tplFilters.Tags = t.Tags
+			} else {
+				return
+			}
+		}
+		var ne *nuclei.NucleiEngine
+		var err error
+		if len(proxies) > 0 {
+			ne, err = nuclei.NewNucleiEngineCtx(context.Background(), nuclei.DisableUpdateCheck(),
+				nuclei.WithTemplateFilters(
+					tplFilters,
+				),
+			)
+		} else {
+			ne, err = nuclei.NewNucleiEngineCtx(context.Background(), nuclei.DisableUpdateCheck(),
+				nuclei.WithTemplateFilters(
+					tplFilters,
+				),
+				nuclei.WithProxy(proxies, true),
+			)
+		}
+		if err != nil {
+			slog.Printf(slog.WARN, "Nuclei new with error:%+v", err)
+			return
+		}
+		defer ne.Close()
+		ne.LoadTargets([]string{t.Url}, false)
+		if err := ne.ExecuteWithCallback(func(event *output.ResultEvent) {
+			resultPipe <- VulnResult{
+				Type: TYPE_NUCLEI,
+				NucleiResults: []*output.ResultEvent{
+					event,
+				},
+			}
+		}); err != nil {
+			slog.Printf(slog.WARN, "Nuclei scan with error:%+v", err)
+		}
 	}
-	if len(t.TemplateIDs) > 0 {
-		tplFilters.IDs = t.TemplateIDs
-	}
-	var ne *nuclei.NucleiEngine
-	var err error
-	if len(t.Proxies) > 0 {
-		ne, err = nuclei.NewNucleiEngineCtx(context.Background(), nuclei.DisableUpdateCheck(),
-			nuclei.WithTemplateFilters(
-				tplFilters,
-			),
-		)
-	} else {
-		ne, err = nuclei.NewNucleiEngineCtx(context.Background(), nuclei.DisableUpdateCheck(),
-			nuclei.WithTemplateFilters(
-				tplFilters,
-			),
-			nuclei.WithProxy(t.Proxies, true),
-		)
-	}
-	if err != nil {
-		slog.Printf(slog.WARN, "Nuclei new with error:%+v", err)
-		return
-	}
-	defer ne.Close()
-	ne.LoadTargets(t.Urls, false)
-	resultPtr := &results
-	if err := ne.ExecuteWithCallback(func(event *output.ResultEvent) {
-		*resultPtr = append(*resultPtr, event)
-	}); err != nil {
-		slog.Printf(slog.WARN, "Nuclei scan with error:%+v", err)
-	}
+}
+
+func (e *Engine) ExecuteCommon(urls []string) (results []*CommonResult) {
 	return
+}
+
+func (e *Engine) ExecuteCPE(items []TargetItem, resultPipe chan VulnResult, progressRate *atomic.Int32) {
+	for index, item := range items {
+		progressRate.Store(int32((index+1)*CPETaskProgressRate) / int32(len(items)))
+		if len(item.CPEs) == 0 {
+			//todo: call nuclei to get cpes
+			continue
+		}
+		var cpeResult = &CPEResult{
+			Url: item.Url,
+		}
+		for _, cpe := range item.CPEs {
+			cpeResult.CVEs = append(cpeResult.CVEs, core.CPE2CVE(cpe)...)
+		}
+		cpeResult.CVEs = utils.RemoveStrSliceDuplicate(cpeResult.CVEs)
+		if len(cpeResult.CVEs) > 0 {
+			go func() {
+				resultPipe <- VulnResult{
+					Type: TYPE_CPE,
+					CPEResults: []*CPEResult{
+						cpeResult,
+					},
+				}
+			}()
+		}
+	}
 }
 
 func (e *Engine) ListTemplates() []*templates.Template {
